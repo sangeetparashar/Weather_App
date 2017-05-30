82244593cf72b31084efe8eada71372e06d153a7
Sangeet Parashar <sparasha@uwo.ca>
2017-05-29 23:24:26 -0400

    learning to use promises and wrapping require inside it

---
+++ Playground/promise-2.js
@@ -0,0 +1,32 @@
+var request = require('request');
+var geocodeAddress = (address) => {
+   
+    return new Promise((resolve, reject) => {
+        var encoded_address = encodeURIComponent(address);
+        request({
+            url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encoded_address}`,
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+                reject('unable to connect to Google server.');
+            }
+            else if (body.status === 'ZERO_RESULTS') {
+                reject('Unable to find the address.');
+            }
+            else if (body.status === 'OK') {
+                //in order to print out the formatted address 
+                resolve({
+                    Address: body.results[0].formatted_address,
+                    Longitude: body.results[0].geometry.location.lng,
+                    Latitude: body.results[0].geometry.location.lat
+                });
+            }
+        });
+    });
+};
+
+geocodeAddress('19146').then((location) => {
+    console.log(JSON.stringify(location, undefined, 2));
+}, (errorMessage) => {
+    console.log(errorMessage);
+});
--- Playground/promise.js
+++ Playground/promise.js
@@ -1,13 +1,35 @@
-var somePromise = new Promise((resolve, reject) => {
-    //benefits of promises is that you have multiple functions to call but don't have to worry about mistakeningly calling it multiple times
-    setTimeout(() => {
-        resolve('Hey, it works!');
-        //reject('Unable to fulfill promise');
-    }, 3000);
+var AsyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('One of them is NOT a number');
+            }
+        }, 2500);
+    });
+};
+
+
+AsyncAdd(4, '5').then((message) => {
+    console.log('Success, meaning the addition of the numbers is', message);
+    return AsyncAdd(message, 9);
+}).then((message) => {
+        console.log('The answer should be 18. The true result is: ', message);
+    }).catch((errorMessage) => {
+    console.log(errorMessage);
 });
 
-somePromise.then((message) => {
-    console.log('Success:', message);
-}, (errorMessage) => {
-    console.log('Printing error:', errorMessage);
-});
+//var somePromise = new Promise((resolve, reject) => {
+//    //benefits of promises is that you have multiple functions to call but don't have to worry about mistakeningly calling it multiple times
+//    setTimeout(() => {
+//        resolve('Hey, it works!');
+//        //reject('Unable to fulfill promise');
+//    }, 3000);
+//});
+
+//somePromise.then((message) => {
+//    console.log('Success:', message);
+//}, (errorMessage) => {
+//    console.log('Printing error:', errorMessage);
+//});
